362cc74e4251fd002dfa7490271030e17a56cc25
Jocelyn Lefrancois <jocelyn.lefrancois@orckestra.com>
2021-07-29 16:11:22 -0400

    Modif 1

--- src/hooks/useFullEntityEditState.js
+++ src/hooks/useFullEntityEditState.js
@@ -16,6 +16,9 @@ export const useFullEntityEditState = (
 
 	const mergedValidationRules = { ...validationRules, ...extendedValidationRules };
 
+  console.log("JOLE MODIF 1");
+  console.log("JOLE MODIF 1.1");
+
 	const isEditStateValid = (newValue, errorTypes, dependencies) => {
 		let error = null;
 		errorTypes.forEach(errorType => {
